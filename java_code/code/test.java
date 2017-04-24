9df022d8526a8fdc2acdc93dfad27492650784b1
Grant <gjamespowell@gmail.com>
2017-04-24 16:12:35 -0400

    updated functions

--- java_code/code/test.java
+++ java_code/code/test.java
@@ -29,32 +29,33 @@ public class test_functions {
     // call this everytime a drink is sold
     public static void add_drink() {
         double randnum = Math.random();
+        int quantity = triangular(1,2,3);
         if (randnum < Budweiser_Probability){
             Budweiser_Sold++;
-            add_to_profit(0)
+            add_to_profit(0, quantity)
         } else if (randnum > (1 - Vodka_Cranberry_Probability)) {
             Vodka_Cranberry_Sold++;
-            add_to_profit(1)
+            add_to_profit(1, quantity)
         } else {
             Sweet_Water_Sold++;
-            add_to_profit(2)
+            add_to_profit(2, quantity)
         }
 
     }
 
     // helper function to add drink
-    public static void add_to_profit(int type_of_drink){
+    public static void add_to_profit(int type_of_drink, int quatitiy){
         double dollars_to_add = 0;
 
         switch(type_of_drink){
             case 0:
-                dollars_to_add = (Budweiser_Price - Budweiser_COGS);
+                dollars_to_add = (Budweiser_Price - Budweiser_COGS) * quatitiy;
                 break;
             case 1;
-                dollars_to_add = (Vodka_Cranberry_Price - Vodka_Cranberry_COGS);
+                dollars_to_add = (Vodka_Cranberry_Price - Vodka_Cranberry_COGS) * quatitiy;
                 break;
             case 2;
-                dollars_to_add = (Sweet_Water_Price - Sweet_Water_COGS);
+                dollars_to_add = (Sweet_Water_Price - Sweet_Water_COGS) * quatitiy;
                 break;
         }
         total_profit += dollars_to_add;
@@ -66,17 +67,13 @@ public class test_functions {
 
         double base_prob = .33333333
 
-        double bw_price_diff_percent = (Budweiser_Price - Budweiser_Base_Price) / Budweiser_Base_Price;
-        double vc_price_diff_percent = (Vodka_Cranberry_Price - Vodka_Cranberry_Base_Price) / Vodka_Cranberry_Base_Price;
-        double sw_price_diff_percent = (Sweet_Water_Price - Sweet_Water_Base_Price) / Sweet_Water_Base_Price;
+        double bw_price_diff_percent = Budweiser_Price / Budweiser_Base_Price;
+        double vc_price_diff_percent = Vodka_Cranberry_Price / Vodka_Cranberry_Base_Price;
+        double sw_price_diff_percent = Sweet_Water_Price / Sweet_Water_Base_Price;
 
         if (bw_price_diff_percent > 2) {bw_price_diff_percent = 2}
         if (vc_price_diff_percent > 2) {vc_price_diff_percent = 2}
         if (sw_price_diff_percent > 2) {sw_price_diff_percent = 2}
-
-        if (bw_price_diff_percent < 0) {bw_price_diff_percent = 0}
-        if (vc_price_diff_percent < 0) {vc_price_diff_percent = 0}
-        if (sw_price_diff_percent < 0) {sw_price_diff_percent = 0}
 
         Budweiser_Probability = (2 - bw_price_diff_percent) * base_prob;
         Vodka_Cranberry_Probability = (2 - vc_price_diff_percent) * base_prob;
